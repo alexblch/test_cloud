263e525d000fc98ddedda4546b5dd385f96ae487
alexblch <bloch.alexandr@gmail.com>
2023-11-23 10:59:17 +0100

    troisieme commit

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const { Sequelize } = require('sequelize')
 // Database
 const sequelize = new Sequelize(
   '', // TODO: database connection string
+  process.env.DATABASE,
   {
     dialect: 'postgres',
     dialectOptions: {
